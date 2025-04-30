4a4e5b4d2df7e660aac8c57d871168df6f54b1bc
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2025-04-30 18:27:39 +0530

    Test script

--- colpali_engine/utils/dataset_transformation.py
+++ colpali_engine/utils/dataset_transformation.py
@@ -17,7 +17,7 @@ def add_metadata_column(dataset, column_name, value):
 def load_train_set() -> DatasetDict:
     ds_path = "colpali_train_set"
     base_path = "./data_dir/" if USE_LOCAL_DATASET else "vidore/"
-    ds_dict = cast(DatasetDict, load_dataset(base_path + ds_path))
+    ds_dict = cast(DatasetDict, load_dataset(base_path + ds_path, split="test"))
     return ds_dict
 
 
@@ -93,7 +93,9 @@ def load_docmatix_ir_negs() -> Tuple[DatasetDict, Dataset, str]:
     ds_dict = DatasetDict({"train": dataset, "test": dataset_eval})
 
     base_path = "./data_dir/" if USE_LOCAL_DATASET else "HuggingFaceM4/"
-    anchor_ds = cast(Dataset, load_dataset(base_path + "Docmatix", "images", split="train"))
+    anchor_ds = cast(
+        Dataset, load_dataset(base_path + "Docmatix", "images", split="train")
+    )
 
     return ds_dict, anchor_ds, "docmatix"
 
@@ -101,7 +103,10 @@ def load_docmatix_ir_negs() -> Tuple[DatasetDict, Dataset, str]:
 def load_wikiss() -> Tuple[DatasetDict, Dataset, str]:
     """Returns the query dataset, then the anchor dataset with the documents, then the dataset type"""
     base_path = "./data_dir/" if USE_LOCAL_DATASET else "Tevatron/"
-    dataset = cast(Dataset, load_dataset(base_path + "wiki-ss-nq", data_files="train.jsonl", split="train"))
+    dataset = cast(
+        Dataset,
+        load_dataset(base_path + "wiki-ss-nq", data_files="train.jsonl", split="train"),
+    )
     # dataset = dataset.select(range(400500))
     dataset_eval = dataset.select(range(500))
     dataset = dataset.select(range(500, len(dataset)))
@@ -168,15 +173,33 @@ def load_train_set_with_docmatix() -> DatasetDict:
 
 def load_docvqa_dataset() -> DatasetDict:
     if USE_LOCAL_DATASET:
-        dataset_doc = cast(Dataset, load_dataset("./data_dir/DocVQA", "DocVQA", split="validation"))
-        dataset_doc_eval = cast(Dataset, load_dataset("./data_dir/DocVQA", "DocVQA", split="test"))
-        dataset_info = cast(Dataset, load_dataset("./data_dir/DocVQA", "InfographicVQA", split="validation"))
-        dataset_info_eval = cast(Dataset, load_dataset("./data_dir/DocVQA", "InfographicVQA", split="test"))
+        dataset_doc = cast(
+            Dataset, load_dataset("./data_dir/DocVQA", "DocVQA", split="validation")
+        )
+        dataset_doc_eval = cast(
+            Dataset, load_dataset("./data_dir/DocVQA", "DocVQA", split="test")
+        )
+        dataset_info = cast(
+            Dataset,
+            load_dataset("./data_dir/DocVQA", "InfographicVQA", split="validation"),
+        )
+        dataset_info_eval = cast(
+            Dataset, load_dataset("./data_dir/DocVQA", "InfographicVQA", split="test")
+        )
     else:
-        dataset_doc = cast(Dataset, load_dataset("lmms-lab/DocVQA", "DocVQA", split="validation"))
-        dataset_doc_eval = cast(Dataset, load_dataset("lmms-lab/DocVQA", "DocVQA", split="test"))
-        dataset_info = cast(Dataset, load_dataset("lmms-lab/DocVQA", "InfographicVQA", split="validation"))
-        dataset_info_eval = cast(Dataset, load_dataset("lmms-lab/DocVQA", "InfographicVQA", split="test"))
+        dataset_doc = cast(
+            Dataset, load_dataset("lmms-lab/DocVQA", "DocVQA", split="validation")
+        )
+        dataset_doc_eval = cast(
+            Dataset, load_dataset("lmms-lab/DocVQA", "DocVQA", split="test")
+        )
+        dataset_info = cast(
+            Dataset,
+            load_dataset("lmms-lab/DocVQA", "InfographicVQA", split="validation"),
+        )
+        dataset_info_eval = cast(
+            Dataset, load_dataset("lmms-lab/DocVQA", "InfographicVQA", split="test")
+        )
 
     # concatenate the two datasets
     dataset = concatenate_datasets([dataset_doc, dataset_info])
@@ -190,10 +213,22 @@ def load_docvqa_dataset() -> DatasetDict:
 
     # create new column image_filename that corresponds to ucsf_document_id if not None, else image_url
     dataset = dataset.map(
-        lambda x: {"image_filename": x["ucsf_document_id"] if x["ucsf_document_id"] is not None else x["image_url"]}
+        lambda x: {
+            "image_filename": (
+                x["ucsf_document_id"]
+                if x["ucsf_document_id"] is not None
+                else x["image_url"]
+            )
+        }
     )
     dataset_eval = dataset_eval.map(
-        lambda x: {"image_filename": x["ucsf_document_id"] if x["ucsf_document_id"] is not None else x["image_url"]}
+        lambda x: {
+            "image_filename": (
+                x["ucsf_document_id"]
+                if x["ucsf_document_id"] is not None
+                else x["image_url"]
+            )
+        }
     )
 
     ds_dict = DatasetDict({"train": dataset, "test": dataset_eval})
